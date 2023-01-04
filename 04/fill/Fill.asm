981d7d5f4d34480336201ae6fe31796418c570e6
unknown <a0952002097@gmail.com>
2023-01-04 21:39:10 +0800

    hw

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,40 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+@SCREEN//將SCREEN 的位置儲存到D
+D=A
+@0
+M=D	//將螢幕起始位置放到RAM0
+
+@KBD //檢查KBD寄存器
+D=M
+@BLACK
+D;JGT	//不為0時跳至@BLACK
+@WHITE
+D;JEQ	//為0時，跳到@WHITE
+@KBDCHECK
+0;JMP
+@1
+M=-1	//WHAT TO FILL SCREEN WITH (-1=11111111111111)
+@CHANGE
+0;JMP
+@1
+M=0	//設定為白屏
+@CHANGE
+0;JMP
+@1	//檢查屏幕
+D=M	//D 為黑屏還是白屏
+@0
+A=M	//GET ADRESS OF SCREEN PIXEL TO FILL
+M=D	//FILL IT
+@0
+D=M+1	//INC 到下個像素
+@KBD
+D=A-D	//KBD-SCREEN=A
+@0
+M=M+1	//INC 到下個像素
+A=M
+@CHANGE
+D;JGT	//IF A=0 EXIT AS THE WHOLE SCREEN IS BLACK
+@RESTART
+0;JMP//無條件跳轉
--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,40 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+@2	//GO TO FINAL ANSWER BOX
+M=0	//
+
+@0
+D=M
+@END
+D;JEQ	//假如D=0
+
+@1
+D=M
+@END
+D;JEQ	//假如D=0
+
+@0	
+D=M	
+@3	
+M=D	
+
+
+(LOOP)
+@1	//獲得第二個號碼
+D=M	
+
+@2	//取得最終值
+M=D+M	
+
+@3	//獲得第一個號碼
+M=M-1	
+
+D=M	
+@LOOP	
+D;JGT	
+
+
+(END)
+@END
+0;JMP	//無限循環
